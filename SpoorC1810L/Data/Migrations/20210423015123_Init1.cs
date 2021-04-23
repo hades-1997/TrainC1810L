048b46a57a28350ec3c71983682af2265308e06d
nhomducjapanese@gmail.com <nhomducjapanese@gmail.com>
2021-04-23 10:58:04 +0700

    module đặt chỗ (admin)

---
+++ SpoorC1810L/Data/Migrations/20210423015123_Init1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpoorC1810L.Migrations
+{
+    public partial class Init1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_trains_stations_StationId",
+                table: "trains");
+
+            migrationBuilder.DropIndex(
+                name: "IX_trains_StationId",
+                table: "trains");
+
+            migrationBuilder.DropColumn(
+                name: "StationId",
+                table: "trains");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StationId",
+                table: "trains",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_trains_StationId",
+                table: "trains",
+                column: "StationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_trains_stations_StationId",
+                table: "trains",
+                column: "StationId",
+                principalTable: "stations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ SpoorC1810L/Models/BookingTicket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainC1810L.Models
+{
+    public class BookingTicket
+    {
+        public int Id { get; set; }
+        public string Field { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Total { get; set; }
+        public DateTime DateOfTravel { get; set; }
+        public string TrainNo { get; set; }
+
+    }
+}
