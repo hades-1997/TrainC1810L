7ac97b5e084f679a0b2bbd309f81b83e60b05690
PC <PC@DESKTOP-J5C13GI>
2021-04-25 12:28:34 +0700

    code booking 1

---
+++ SpoorC1810L/Models/TemporaryBooking.cs
@@ -0,0 +1,19 @@
+﻿using SpoorC1810L.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainC1810L.Models
+{
+    public class TemporaryBooking
+    {
+        public int Id { get; set; }
+        public int TrainsId { get; set; }
+        public int Trainno { get; set; }
+        public string Name { get; set; }
+        public int total { get; set; }
+        public int Seats { get; set; }
+
+    }
+}
